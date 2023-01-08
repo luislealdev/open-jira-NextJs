41169debe0406cde2a7c55a56966a46dc565aad1
Luis Leal <luisrrleal@gmail.com>
2023-01-08 10:11:53 -0600

    Connect MongoDB with mongoose

---
+++ database/db.ts
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+/*
+* 0 = disconnected
+* 1 = connected
+* 2 = connecting
+* 3 = disconnecting
+*/
+
+const mongoConnection = {
+    isConnected: 0
+}
+
+export const connect = async () => {
+    if (mongoConnection.isConnected) {
+        console.log('Already connected');
+        return;
+    }
+
+    if (mongoose.connections.length > 0) {
+        mongoConnection.isConnected = mongoose.connections[0].readyState;
+
+        if (mongoConnection.isConnected === 1) {
+            console.log('Using anterior connection');
+            return;
+        }
+
+        await mongoose.disconnect();
+    }
+
+    await mongoose.connect('...');
+    mongoConnection.isConnected = 1;
+    console.log('Connected to mongoDB', '');
+}
+
+export const disconnect = async () => {
+    if (mongoConnection.isConnected !== 0) return;
+
+    await mongoose.disconnect();
+    console.log('Disconnected from mongoDB');
+
+}
