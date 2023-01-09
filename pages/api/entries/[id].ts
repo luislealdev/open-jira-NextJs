31a8916e159276060343d2917a6eb1d480fd0970
Luis Leal <luisrrleal@gmail.com>
2023-01-09 11:42:56 -0600

    Update endpoint of update

--- pages/api/entries/[id].ts
+++ pages/api/entries/[id].ts
@@ -28,9 +28,15 @@ const updateEntry = async (req: NextApiRequest, res: NextApiResponse) => {
 
     const { description = entryToUpdate!.description, status = entryToUpdate!.status } = req.body;
 
-    const updatedEntry = await Entry.findByIdAndUpdate(id, { description, status }, { runValidators: true, new: true });
+    try {
+        const updatedEntry = await Entry.findByIdAndUpdate(id, { description, status }, { runValidators: true, new: true });
+        res.status(200).json(updatedEntry);
+        await db.disconnect();
+    } catch (error: any) {
+        console.log(error);
+        await db.disconnect();
+        res.status(400).json({ message: error.erros.status.message });
+    }
 
-    res.status(200).json(updatedEntry);
 
-    await db.disconnect();
 }
