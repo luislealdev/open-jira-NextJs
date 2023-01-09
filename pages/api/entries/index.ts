e3e8d69d98fec48880ad66eaf9c36efebc56d155
Luis Leal <luisrrleal@gmail.com>
2023-01-08 22:27:23 -0600

    Create new entry with backend

--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -3,6 +3,7 @@ import { IEntry } from '../../../models/Entry';
 import { db } from '../../../database';
 import { Entry } from '../../../models';
 
+
 type Data = | { message: string } | IEntry[] | IEntry;
 
 export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
@@ -11,6 +12,9 @@ export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
     switch (req.method) {
         case 'GET':
             return getEntries(res);
+
+        case 'POST':
+            return addEntry(req, res);
 
         default:
             return res.status(400).json({ message: 'Not entry point' });
@@ -23,4 +27,30 @@ const getEntries = async (res: NextApiResponse<Data>) => {
     db.disconnect();
 
     res.status(200).json(entries);
+}
+
+const addEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+
+    const { description } = req.body;
+
+
+    const newEntry = new Entry({
+        description,
+        createdAt: new Date()
+    });
+
+
+    try {
+        await db.connect();
+
+        await newEntry.save();
+
+        await db.disconnect();
+
+        return res.status(201).json(newEntry);
+    } catch (error) {
+        await db.disconnect();
+
+        return res.status(500).json({ message: 'Ops! Something went wrong, check console log' });
+    }
 }
