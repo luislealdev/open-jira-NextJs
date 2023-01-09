1382995c5dd3f5fd3cb535fa67f417a6632a555a
Luis Leal <luisrrleal@gmail.com>
2023-01-08 23:49:28 -0600

    Update an entry-backend

---
+++ pages/api/entries/[id].ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { IEntry } from '../../../models/Entry';
+import mongoose from 'mongoose';
+import { db } from '../../../database';
+import { Entry } from '../../../models';
+
+type Data = { message: string } | IEntry
+
+export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
+    const { id } = req.query;
+
+    if (!mongoose.isValidObjectId(id)) return res.status(400).json({ message: `The id: ${id}, is not valid` });
+
+    switch (req.method) {
+        case 'PUT':
+            return updateEntry(req, res);
+
+        default:
+            return res.status(400).json({ message: 'Method doesnt exist' });
+    }
+}
+
+const updateEntry = async (req: NextApiRequest, res: NextApiResponse) => {
+    const { id } = req.query;
+
+    await db.connect();
+    const entryToUpdate = await Entry.findById(id);
+
+    const { description = entryToUpdate!.description, status = entryToUpdate!.status } = req.body;
+
+    const updatedEntry = await Entry.findByIdAndUpdate(id, { description, status }, { runValidators: true, new: true });
+
+    res.status(200).json(updatedEntry);
+
+    await db.disconnect();
+}
